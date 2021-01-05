e3a04ade060d0e7cf386638cb279c13b6796c82d
douglasz1 <douglas.santana.dev@gmail.com>
2021-01-05 10:44:13 -0300

    :heavy_plus_sign:

--- app/behavior.py
+++ app/behavior.py
@@ -5,7 +5,7 @@ from .models import database
 def _db_connect():
     if database.is_closed():
         database.connect()
-
+#those credentials from each transformations will be build into here
 @app.teardown_request
 def _db_close(exc):
     if not database.is_closed():
